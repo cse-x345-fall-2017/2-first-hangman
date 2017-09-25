683c28d78a4aa9c4a715b4b7e3758ab5304a18a2
Anthony Myers <amyers@smu.edu>
2017-09-25 11:44:24 -0400

    added additional tests

--- hangman/test/hangman_test.exs
+++ hangman/test/hangman_test.exs
@@ -5,20 +5,25 @@ defmodule HangmanTest do
   
   test "new game" do
     game = Hangman.new_game()
-    assert game.used == []
-    assert game.turns_left == 7
-    assert game.last_guess == ""
+    assert %{used: [], turns_left: 7, last_guess: ""} = game
     
+    tally = Hangman.tally(game)
+    assert %{game_state: :initializing, turns_left: 7, last_guess: ""} = tally
+  end
+    
+    
+  test "make move(s)" do
+    game = Hangman.new_game()
+  
     guess1 = "a"
     {game, _tally} = Hangman.make_move(game, guess1)
-    assert game.used == [guess1]
-    assert game.last_guess == guess1
+    assert %{used: [^guess1], last_guess: ^guess1} = game
     
     guess2 = "b"
     {game, _tally} = Hangman.make_move(game, guess2)
     assert guess1 in game.used
     assert guess2 in game.used
-    assert game.last_guess == guess2
-    
+    assert %{last_guess: ^guess2} = game  
   end
+  
 end
