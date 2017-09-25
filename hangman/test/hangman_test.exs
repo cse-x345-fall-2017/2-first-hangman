015ce9c38b9401d4be97e83ce9a67ae3fa338e67
Anthony Myers <amyers@smu.edu>
2017-09-24 22:01:10 -0400

    added a test

--- hangman/test/hangman_test.exs
+++ hangman/test/hangman_test.exs
@@ -1,8 +1,24 @@
 defmodule HangmanTest do
   use ExUnit.Case
   doctest Hangman
-
-  test "greets the world" do
-    assert Hangman.hello() == :world
+  
+  
+  test "new game" do
+    game = Hangman.new_game()
+    assert game.used == []
+    assert game.turns_left == 7
+    assert game.last_guess == ""
+    
+    guess1 = "a"
+    {game, _tally} = Hangman.make_move(game, guess1)
+    assert game.used == [guess1]
+    assert game.last_guess == guess1
+    
+    guess2 = "b"
+    {game, _tally} = Hangman.make_move(game, guess2)
+    assert guess1 in game.used
+    assert guess2 in game.used
+    assert game.last_guess == guess2
+    
   end
 end
