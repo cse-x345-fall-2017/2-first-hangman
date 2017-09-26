75511393cbd8842eda8c33988e33a1062444567a
Luke Wood <lukewoodcs@gmail.com>
2017-09-26 18:13:49 -0500

    Tests

--- hangman/lib/game.ex
+++ hangman/lib/game.ex
@@ -20,24 +20,25 @@ defmodule Hangman.Game do
     }
   end
 
+  # Exists for testing purposes
+  def new_game(word) do
+    %State{
+      target:  word,
+      letters: List.duplicate("_", String.length(word))
+    }
+  end
+
   def tally(game) do
     %{
       game_state: game.game_state
     }
   end
 
-  def make_move(game, guess) when guess =~ Map.get(game, :target) do
-    new_game = %{
-      game
-    }
-    {game, Hangman.Game.tally(game)}
+  def make_move(game = %{used: used}, guess) do
+    {%{game | game_state: :already_used}, Hangman.Game.tally(game)}
   end
 
-  def make_move(game, guess) do
-    new_game = %{
-      game
-    }
+  def make_move(game = %{target: target}, guess) do
     {game, Hangman.Game.tally(game)}
   end
-
 end
--- hangman/test/hangman_test.exs
+++ hangman/test/hangman_test.exs
@@ -19,4 +19,45 @@ defmodule HangmanTest do
     game = Hangman.new_game()
     {game, tally} = Hangman.make_move(game, "a")
   end
+
+  test "makes a move twice" do
+    game = Hangman.new_game()
+    {game, tally} = Hangman.make_move(game, "a")
+    {game, tally} = Hangman.make_move(game, "a")
+    assert game.state == :already_used
+  end
+
+  test "makes a good guess" do
+    game = Hangman.new_game("cats")
+    {game, tally} = Hangman.make_move(game, "a")
+    assert game.state == :good_guess
+  end
+
+  test "makes a bad guess" do
+    game = Hangman.new_game("cats")
+    {game, tally} = Hangman.make_move(game, "z")
+    assert game.state == :bad_guess
+  end
+
+  test "wins the game" do
+    game = Hangman.new_game("cats")
+    {game, tally} = Hangman.make_move(game, "c")
+    {game, tally} = Hangman.make_move(game, "a")
+    {game, tally} = Hangman.make_move(game, "t")
+    {game, tally} = Hangman.make_move(game, "s")
+    assert game.state == :won
+  end
+
+  test "loses the game" do
+    game = Hangman.new_game("cats")
+    {game, tally} = Hangman.make_move(game, "c")
+    {game, tally} = Hangman.make_move(game, "w")
+    {game, tally} = Hangman.make_move(game, "t")
+    {game, tally} = Hangman.make_move(game, "b")
+    {game, tally} = Hangman.make_move(game, "d")
+    {game, tally} = Hangman.make_move(game, "e")
+    {game, tally} = Hangman.make_move(game, "f")
+    assert game.state == :won
+  end
+
 end
