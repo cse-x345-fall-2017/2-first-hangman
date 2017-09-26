b915af0f1294369250c87d3918cd2ed77b9f77ab
Luke Wood <lukewoodcs@gmail.com>
2017-09-26 17:28:42 -0500

    Added some tests

---
+++ hangman/lib/game.ex
@@ -0,0 +1,17 @@
+defmodule Hangman.Game do
+  @moduledoc """
+  Documentation for Hangman.
+  """
+
+  def new_game() do
+  end
+
+  def tally(game) do
+
+  end
+
+  def make_move(game, guess)do
+
+  end
+
+end
