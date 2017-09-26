42c5d906606b359b8db8094c0ac6c90ced158c63
Luke Wood <lukewoodcs@gmail.com>
2017-09-26 17:28:41 -0500

    Added some tests

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -3,17 +3,8 @@ defmodule Hangman do
   Documentation for Hangman.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Hangman.hello
-      :world
-
-  """
   defdelegate new_game(), to: Hangman.Game
   defdelegate tally(game), to: Hangman.Game
   defdelegate make_move(game, guess), to: Hangman.Game
-
+  
 end
--- hangman/test/hangman_test.exs
+++ hangman/test/hangman_test.exs
@@ -5,4 +5,18 @@ defmodule HangmanTest do
   test "the truth" do
     assert 1 + 1 == 2
   end
+
+  test "creates a game" do
+    Hangman.new_game()
+  end
+
+  test "gets a tally" do
+    game = Hangman.new_game()
+    Hangman.tally(game)
+  end
+
+  test "makes a move" do
+    game = Hangman.new_game()
+    {game, tally} = Hangman.make_move(game, "a")
+  end
 end
