702a7262434ee7f829b778a2ab6be26d07d29d3b
Luke Wood <lukewoodcs@gmail.com>
2017-09-26 17:23:27 -0500

    TEST

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -12,7 +12,8 @@ defmodule Hangman do
       :world
 
   """
-  def hello do
-    :world
-  end
+  defdelegate new_game(), to: Hangman.Game
+  defdelegate tally(game), to: Hangman.Game
+  defdelegate make_move(game, guess), to: Hangman.Game
+
 end
