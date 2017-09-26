cb1d5d2eb1ebf58b2c1a161dd10c3dd4079d477a
Luke Wood <lukewoodcs@gmail.com>
2017-09-26 17:40:08 -0500

    new game works

--- hangman/lib/game.ex
+++ hangman/lib/game.ex
@@ -1,9 +1,22 @@
+defmodule State do
+    defstruct game_state: :initializing,
+     turns_left: 7,
+     letters: [],
+     used: [],
+     target: ""
+end
+
 defmodule Hangman.Game do
   @moduledoc """
   Documentation for Hangman.
   """
 
   def new_game() do
+    word = Dictionary.random_word()
+    %State{
+      target:  word,
+      letters: List.duplicate("_", String.length(word))
+    }
   end
 
   def tally(game) do
@@ -11,7 +24,7 @@ defmodule Hangman.Game do
   end
 
   def make_move(game, guess)do
-
+    {1, 2}
   end
 
 end
