271aa7dc2dc33dc0a08764a18f0b75472e252623
Justin Ledford <justinledford@gmail.com>
2017-09-28 15:22:04 -0500

    start make_move

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -18,6 +18,24 @@ defmodule Hangman do
     state |> Map.from_struct |> Map.delete(:word)
   end
 
+  def make_move(state, letter) do
+    # Decrease turns_left,
+    # add letter to used
+    # add letter to last_guess
+    # update letters
+    # update game_state
+    letter_in_word? = letter in String.codepoints(State.word)
+    state
+    |> Map.update!(:turns_left, &(&1 - 1))
+  end
+
+  def update_state(state, letter, true) do
+
+  end
+  def update_state(state, letter, false) do
+
+  end
+
   def word_to_letters(word, used) do
     word |> String.codepoints |> Enum.map(&(if &1 in used do &1 else "_" end))
   end
