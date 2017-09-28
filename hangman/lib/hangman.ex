46c8c6b17d8ffdb06ba623660c5ca0275d8f881c
Justin Ledford <justinledford@gmail.com>
2017-09-28 12:14:56 -0500

    add tally and new game

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -4,9 +4,21 @@ defmodule Hangman do
   """
 
   defstruct turns_left: 7, game_state: :initializing,
-            word: "", used: [], last_guess: ''
+            word: "", used: [], last_guess: '', letters: []
 
   def new_game do
-    %Hangman{ word: Dictionary.random_word() }
+    word = Dictionary.random_word()
+    letters = for _ <- String.codepoints(word), do: "-"
+    %Hangman{}
+    |> Map.put(:word, word)
+    |> Map.put(:letters, letters)
+  end
+
+  def tally(state) do
+    state |> Map.from_struct |> Map.delete(:word)
+  end
+
+  def word_to_letters(word, used) do
+    word |> String.codepoints |> Enum.map(&(if &1 in used do &1 else "_" end))
   end
 end
