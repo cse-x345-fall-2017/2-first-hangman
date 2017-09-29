83f581cb8a5fa4eb82dd94e8cea18fb45144775b
aWiedebusch <awiedebusch@smu.edu>
2017-09-29 13:44:44 -0500

    Works as Intended, unoptimal

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -5,7 +5,7 @@ defmodule Hangman.Game do
         game_state: :initializing,
         word: [],
         letters: [],
-        used: MapSet.new,
+        used: [],
         last_guess: ""
     )
 
@@ -25,10 +25,10 @@ defmodule Hangman.Game do
 #####################################################################
 
     def tally(game) do 
-        %{game_state: status, turns_left: left, letters: h,
+        %{game_state: status, turns_left: left, letters: l,
             used: u, last_guess: g} = game
 
-        %{game_state: status, turns_left: left, letters: h,
+        %{game_state: status, turns_left: left, letters: l,
             used: u, last_guess: g}
     end
 
@@ -36,68 +36,87 @@ defmodule Hangman.Game do
 
     def make_move(game, guess) do
 
-        %{word: w, letters: h, used: u, turns_left: tl} = game
+        guess_ok?(Regex.match?(~r/[a-z]/, guess))
 
-        {game_state, new_h} = guess_attempt(
+        %{word: w, letters: l, used: u, turns_left: tl} = game
+
+        {game_state, new_l, u} = 
+        guess_attempt(
             Enum.any?(w,fn(x) -> x == guess end),
             Enum.any?(u,fn(x) -> x == guess end),
             w,
-            h,
+            l,
+            u,
             guess
-        )
+        ) 
 
-        {game_state, tl} = game_check(
+        {game_state, tl, new_l} = 
+        game_check(
             game_state,
+            w,
+            new_l,
             tl <= 1,
             tl,
-            w == new_h
-        )
+            w == new_l
+        ) 
 
         {
             %{ 
                 game_state: game_state,
                 turns_left: tl,
                 word: w,
-                letters: new_h,
-                used: MapSet.union([guess] |> Enum.into(MapSet.new), u),
+                letters: new_l,
+                used: u,
                 last_guess: guess
             },
             %{
                 game_state: game_state,
                 turns_left: tl,
-                letters: new_h,
-                used: MapSet.union([guess] |> Enum.into(MapSet.new), u),
+                letters: new_l,
+                used: u,
                 last_guess: guess
             }
         }
     end
 
-    defp guess_attempt(_, true, _, h, _),         do: {:already_used, h}
-    defp guess_attempt(false, _, _, h, _),        do: {:bad_guess, h}
-    defp guess_attempt(true, false, w, h, guess), do: insert_guess(w,h,guess, true)
+    defp guess_ok?(false), do: raise "Please enter an English alphabet character"
+    defp guess_ok?(_), do: :ok
+
+    defp guess_attempt(_, true, _, l, u, _),         do: {:already_used, l, u}
+    defp guess_attempt(false, _, _, l, u, guess),    do: {:bad_guess, l, format_used(u,guess)}
+    defp guess_attempt(true, false, w, l, u, guess), do: insert_guess(w, l, guess, true, u)
 
     #:good_guess passed, function below will always insert into
     #letters field at least once
 
-    defp insert_guess(w, h, guess, _) do 
+    defp insert_guess(_, l, guess, nil, u),          do: {:good_guess, l, format_used(u,guess)}
+    defp insert_guess(w, l, guess, _, u) do 
     
         index = Enum.find_index(w, fn(x) -> x == guess end)
 
-        h = List.replace_at(h, index, guess)
-        {_, w} = List.pop_at(w,index)
+        l = List.replace_at(l, index, guess)
+        w = List.replace_at(w,index, "!")
 
-        insert_guess(w, h, guess, Enum.find_index(w, fn(x) -> x == guess end))
+        insert_guess(w, l, guess, Enum.find_index(w, fn(x) -> x == guess end), u)
     end
-    defp insert_guess(_, h, _    , nil), do: {:good_guess, h}
 
-    defp game_check(:already_used, _, tl, _),   do: {:already_used, tl}
+    #Changes state of game based on the guess check
 
-    defp game_check(:bad_guess, false, tl, _),  do: {:bad_guess, tl-1}
-    defp game_check(:bad_guess, true, tl, _),   do: {:lost, tl-1}
+    defp game_check(:already_used, _, l, _, tl, _),   do: {:already_used, tl, l}
 
-    defp game_check(:good_guess, _, tl, false), do: {:good_guess, tl}
-    defp game_check(:good_guess, _, tl, true),  do: {:won, tl}
+    defp game_check(:bad_guess, _, l, false, tl, _),  do: {:bad_guess, tl-1, l}
+    defp game_check(:bad_guess, w, _, true, tl, _),   do: {:lost, tl-1, w}
+
+    defp game_check(:good_guess, _, l, _, tl, false), do: {:good_guess, tl, l}
+    defp game_check(:good_guess, w, _, _, tl, true),  do: {:won, tl, w}
     
+
+    defp format_used(used, guess) do
+        (used ++ [guess])
+        |> Enum.into(MapSet.new)
+        |> MapSet.to_list()
+
+    end
 
 
 end
