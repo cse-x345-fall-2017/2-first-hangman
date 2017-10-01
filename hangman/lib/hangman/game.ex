e36bdc65809f04e351d4e5076c53d20fd9cf1ca0
Kellen Schmidt <kellenschmidt31@gmail.com>
2017-09-30 21:18:31 -0500

    Defined struct and new_game

---
+++ hangman/lib/hangman/game.ex
@@ -0,0 +1,17 @@
+defmodule Hangman.Game do
+
+  defstruct game_state: :initializing,   # :won | :lost | :already_used | :good_guess | :bad_guess | :initializing
+            turns_left: 7,               # the number of turns left (game starts with 7)
+            letters: [],                 # a list of single character strings. If a letter in a particular
+                                         # position has been guessed, that letter will appear in `letters`. 
+                                         # Otherwise, it will be shown as an underscore
+            used: [],                    # A sorted list of the letters already guessed
+            last_guess: ""               # the last letter guessed by the player
+
+  def new_game() do
+    %Hangman.Game{
+      letters: ["K","E","L","L","E","N"]
+    }
+  end
+    
+end
