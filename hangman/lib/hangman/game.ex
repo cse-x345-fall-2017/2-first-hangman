9a21a84a2858e0dd4bba67faf28097f7d72172f6
Kellen Schmidt <kellenschmidt31@gmail.com>
2017-10-01 23:58:27 -0500

    Break into smaller functions

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -13,7 +13,7 @@ defmodule Hangman.Game do
     word = Dictionary.random_word
     %Hangman.Game{
       word: word,
-      letters: List.duplicate("_", String.length(word))
+      letters: get_underscore_list(word)
     }
   end
 
@@ -31,7 +31,12 @@ defmodule Hangman.Game do
     letter_is_used = guess in used
     letters_unguessed = "_" in letters
     valid_letter = String.match?(guess, ~r/^[a-z]$/)
+
     do_guess(game, letter_in_word, letter_is_used, letters_unguessed, valid_letter)
+  end
+
+  defp get_underscore_list(word) do
+    List.duplicate("_", String.length(word))
   end
 
   defp get_letter_representation(letter, true) do
@@ -42,6 +47,21 @@ defmodule Hangman.Game do
     "_"
   end
 
+  defp set_game_state(state, new_game_state) do
+    state
+    |> Map.put(:game_state, new_game_state)
+  end
+
+  defp add_letter_to_used(state = %{used: used, last_guess: last_guess}) do
+    state
+    |> Map.put(:used, used ++ [last_guess])
+  end
+
+  defp decrement_turns(state = %{turns_left: turns_left}) do
+    state
+    |> Map.put(:turns_left, turns_left-1)
+  end
+
   defp do_guess(_, _, _, _, false) do
     raise ArgumentError, message: "Invalid letter, must be a-z"
   end
@@ -49,24 +69,25 @@ defmodule Hangman.Game do
   # Letter already used
   defp do_guess(state, _, true, _, _) do
     game = state 
-    |> Map.put(:game_state, :already_used)
+    |> set_game_state(:already_used)
 
     { game, tally(game) }
   end
 
   # Letter is in word and not used, game not over
-  defp do_guess(state = %{used: used, last_guess: last_guess}, true, false, true, _) do
+  defp do_guess(state, true, false, true, _) do
     game = state 
-    |> Map.put(:game_state, :good_guess)
-    |> Map.put(:used, used ++ [last_guess])
+    |> set_game_state(:good_guess)
+    |> add_letter_to_used
     |> add_decoded_letters_list
 
     { game, tally(game) }
   end
 
+  # Last guess and letter not in word -> game over
   defp do_guess(state = %{turns_left: 1}, false, _, _, _) do
     game = state 
-    |> Map.put(:game_state, :lost)
+    |> set_game_state(:lost)
     |> Map.put(:turns_left, 0)
 
     { game, tally(game) }
@@ -75,9 +96,9 @@ defmodule Hangman.Game do
   # Letter is not in word
   defp do_guess(state = %{used: used, turns_left: turns_left, last_guess: last_guess}, false, _, _, _) do
     game = state 
-    |> Map.put(:game_state, :bad_guess)
-    |> Map.put(:turns_left, turns_left-1)
-    |> Map.put(:used, used ++ [last_guess])
+    |> set_game_state(:bad_guess)
+    |> decrement_turns
+    |> add_letter_to_used
 
     { game, tally(game) }
   end
@@ -85,13 +106,16 @@ defmodule Hangman.Game do
   # Game over, all letters in word guessed
   defp do_guess(state, _, _, false, _) do
     game = state 
-    |> Map.put(:game_state, :won)
+    |> set_game_state(:won)
 
     { game, tally(game) }
   end
 
   defp add_decoded_letters_list(state = %{word: word}) do
-    new_letters = word |> String.codepoints |> Enum.map(&(get_letter_representation(&1, &1 in state.used)))
+    new_letters = word 
+    |> String.codepoints 
+    |> Enum.map(&(get_letter_representation(&1, &1 in state.used)))
+    
     state
     |> Map.put(:letters, new_letters)
     |> check_victory("_" in new_letters)
@@ -99,7 +123,7 @@ defmodule Hangman.Game do
 
   defp check_victory(state, false) do
     state
-    |> Map.put(:game_state, :won)
+    |> set_game_state(:won)
   end
 
   defp check_victory(state, true) do
