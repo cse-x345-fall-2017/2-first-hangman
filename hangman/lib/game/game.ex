9993ec7aff19a5c44097d6f0dff421c0ea5d133a
Aditya Rao <adirlzr@gmail.com>
2017-10-06 17:16:40 -0500

    Evaluate function works correctly (verified by test)

--- hangman/lib/game/game.ex
+++ hangman/lib/game/game.ex
@@ -1,19 +1,13 @@
 defmodule Hangman.Game do
   def new_game() do 
     # function to filter alphabets
-    alphabets_only = fn x -> if Regex.match?(~r{[a-zA-Z]}, x),
-                               do: true,
-                               else: false
-                            end
-
+    alphabets_only = fn x -> Regex.match?(~r{[a-zA-Z]}, x) end
     # Get a random word from dictionary and store it as a list of characters
     word = Dictionary.random_word()
             |> String.split("")
             |> Enum.filter(alphabets_only)
-
     # Initialize the progress as blanks
     letters =  Enum.map(word, fn _letter  -> "_" end)
-
     %State{
       game_state: :initializing,
       turns_left: 7,
@@ -24,64 +18,45 @@ defmodule Hangman.Game do
     }
   end
 
-  def tally(game) do
-    game
-      |> Map.from_struct()
-      |> Map.drop([:word])
+  def tally(%State{ game_state: state,
+                    turns_left: turns,
+                    letters: letters,
+                    used: used})
+  do
+    %{
+      game_state: state,
+      turns_left: turns,
+      letters: letters,
+      used: used
+    }
   end
 
-  def make_move(game, guess) do
-    if Enum.member?(game.used, guess) do
-      game = %State{ game |
-        game_state: :already_guessed
-      }
-      { game, tally(game) }
-    else
-      make_move(game, guess, :new_guess)
-    end
+  def evaluate(guess, word, letters), do: evaluate(guess,
+                                                   word,
+                                                   letters,
+                                                   [])
+  # First alphabet matches guess
+  def evaluate(guess, [ guess | word_tail ], [ _alphabet | letters_tail ], letters_new) do
+    evaluate(guess, word_tail, letters_tail, letters_new |> Enum.concat([guess]))
   end
-  def make_move(game, guess, _new_guess) do
-    used = Enum.concat(game.used, [guess])
-    letters_after = check(game.word, guess, game.letters)
-    
-    letters_changed? = letters_after !=  game.letters
-    won? = letters_after == game.word
-
-    
-    { game_state, turns_left } = update_if(letters_changed?,
-                                           game.turns_left,
-                                           won?)
-
-    # Letters represents entire word if won or lost
-    letters_after = if [:won, :lost] |> Enum.member?(game_state)
-                    do
-                      [Enum.join(game.word, "")]
-                    else
-                      letters_after
-                    end
-
-    updated_game = %State{ game | used: used,
-                                  letters: letters_after,
-                                  game_state: game_state,
-                                  turns_left: turns_left,
-                                  last_guess: guess }
-    { updated_game, tally(updated_game) }
+  # When the alphabet at the position in word has already been guessed right
+  def evaluate(guess, [ alphabet | word_tail ], [ alphabet | letters_tail ], letters_new)
+  do
+    evaluate(guess, word_tail, letters_tail, letters_new |> Enum.concat([alphabet]))
   end
 
-  # Returns updated values of turns and game state
-  def update_if(_letters_changed?, turns_left, true), do: { :won, turns_left }
-  def update_if(true, turns_left, _won), do: { :good_guess, turns_left }
-  def update_if(false, 1, _won), do: { :lost, 0 }
-  def update_if(false, turns_left, _won), do: { :bad_guess, turns_left-1 }
+  # When guess matches the current alphabet
+  def evaluate(guess, [ guess | word_tail ], [ "_" | letters_tail ], letters_new)
+  do
+    evaluate(guess, word_tail, letters_tail, letters_new |> Enum.concat([guess]))
+  end
 
-  # Compares guess with word to update letters
-  def check(word, guess, letters), do: check(word, guess, letters, 0)
-  def check([ guess | tail ], guess, letters, index) do
-    letters_new = List.replace_at(letters, index, guess)
-    check(tail, guess, letters_new, index+1)
+  # When guess does not match the current alphabet
+  def evaluate(guess, [ _alphabet | word_tail ], [ "_" | letters_tail ], letters_new)
+  do
+    evaluate(guess, word_tail, letters_tail, letters_new |> Enum.concat(["_"]))
   end
-  def check([ _head | tail ], guess, letters, index) do
-    check(tail, guess, letters, index+1)
-  end
-  def check([], _guess, letters, _index), do: letters
+
+  # No alphabets left
+  def evaluate(_guess, [], _letters, letters_new), do: letters_new
 end
--- hangman/test/game/game_test.exs
+++ hangman/test/game/game_test.exs
@@ -1,16 +1,30 @@
 defmodule GameTest do
   use ExUnit.Case
 
-  test "guess is checked correctly" do
-    # Using test word "elba"
-    game = Hangman.new_game()
-    game = %State{game |
-    word: ["e", "l", "b", "a"],
-    letters: ["_", "_", "_", "_"]
-    }
+  # test "guess is evaluated correctly" do
+  #   # Using test word "elba"
+  #   game = Hangman.new_game()
+  #   game = %State{game |
+  #   word: ["e", "l", "b", "a"],
+  #   letters: ["_", "_", "_", "_"]
+  #   }
 
-    assert ["e", "_", "_", "_"] == Hangman.Game.check(game.word,
-                                                     "e",
-                                                     game.letters)       
+  #   assert ["e", "_", "_", "_"] == Hangman.Game.check(game.word,
+  #                                                    "e",
+  #                                                    game.letters)       
+  # end
+
+  test " guess is evaluated correctly" do
+    # word is peeve, guess is 'e'
+    word = ["p","e", "e", "v", "e"]
+    guess = "e"
+    letters = ["_","_", "_", "_", "_"]
+    result_1 =  Hangman.Game.evaluate(guess, word, letters)
+
+    assert result_1 == ["_", "e", "e", "_", "e"]
+
+    result_2 = Hangman.Game.evaluate("p", word, result_1)
+
+    assert result_2 == ["p", "e", "e", "_", "e"]
   end
 end
